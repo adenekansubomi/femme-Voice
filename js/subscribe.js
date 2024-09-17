a4a831f3ba3386e5f35976fd55bde77e422f4ecd
adenekansubomi <129598884+adenekansubomi@users.noreply.github.com>
2024-09-17 14:29:06 +0100

    Update subscribe.js

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,9 +1,26 @@
 
-        document.addEventListener('DOMContentLoaded', function() {
-            var subscribeButton = document.getElementById('subscribeButton');
+        // document.addEventListener('DOMContentLoaded', function() {
+        //     var subscribeButton = document.getElementById('subscribeButton');
             
+        //     subscribeButton.addEventListener('click', function(event) {
+        //         event.preventDefault(); // Prevent the default action of the link
+        //         alert("You have successfully subscribed");
+        //     });
+        // });
+
+document.addEventListener('DOMContentLoaded', function() {
+            var subscribeButton = document.getElementById('subscribeButton');
+            var emailInput = document.getElementById('emailInput');
+
             subscribeButton.addEventListener('click', function(event) {
                 event.preventDefault(); // Prevent the default action of the link
-                alert("You have successfully subscribed");
+                
+                var email = emailInput.value; // Get the value from the input
+                
+                if (email) { // Check if the email input is not empty
+                    alert('Thank you for subscribing with ' + email + '!');
+                } else {
+                    alert('Please enter a valid email address.');
+                }
             });
         });
