95444a0bda0bccbac4645a9bcd581e1195abdf57
adenekansubomi <129598884+adenekansubomi@users.noreply.github.com>
2024-09-17 14:50:13 +0100

    Update subscribe.js

--- js/subscribe.js
+++ js/subscribe.js
@@ -16,10 +16,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 event.preventDefault(); // Prevent the default action of the link
                 
                 var email = emailInput.value; // Get the value from the input
-                var atIndex = email.indexOf("@");
-                var com = email.indexOf(".com")
+                var atIndex = emailInput.indexOf("@");
+                var com = emailInput.indexOf(".com")
                 
-                if (atIndex == -1 || com == -1 || email.length <= 8) { // Check if the email input is not empty
+                if (atIndex == -1 || com == -1 || emailInput.length <= 8) { // Check if the email input is not empty
                     alert('Thank you for subscribing with femmeVoice);
                 } else {
                     alert('Please enter a valid email address.');
