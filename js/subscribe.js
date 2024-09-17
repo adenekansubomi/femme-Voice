442f2d32cc57cbb3e81163377a0fe911baced329
adenekansubomi <129598884+adenekansubomi@users.noreply.github.com>
2024-09-17 14:06:56 +0100

    Update subscribe.js

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,10 +1,9 @@
 
         document.addEventListener('DOMContentLoaded', function() {
             var subscribeButton = document.getElementById('subscribeButton');
-            var confirmationMessage = document.getElementById('confirmationMessage');
             
             subscribeButton.addEventListener('click', function(event) {
                 event.preventDefault(); // Prevent the default action of the link
-                confirmationMessage.style.display = 'block'; // Show the confirmation message
+                alert("You have successfully subscribed");
             });
         });
